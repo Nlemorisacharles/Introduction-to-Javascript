093883c56846df85e4981dce367fcd4f18e35de6
Nlemorisacharles <105318981+Nlemorisacharles@users.noreply.github.com>
2022-06-22 14:41:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-/* Second Javascript task 1 */
+
 const num1 = parseFloat(prompt("Enter first number"))
 const num2 = parseFloat(prompt("Enter second number"))
 const opr =  prompt("Enter operator from the list below +, -, *, /")
-let result = 0
+let result = 0 
 
 if(isNaN(num1) || isNaN(num2)){
-    alert('Fill all inputs! Enter number values!')
+    alert('Please ensure all inputs are fielded and all values must be numbers')
 }else{
     if(opr){
         if(opr == '+'){
@@ -13,7 +13,7 @@ if(isNaN(num1) || isNaN(num2)){
             alert('Your answer is ' + result)
         }
         else if(opr == '-'){
-             result = num1 - num2;
+            result = num1 - num2;
             alert('Your answer is ' + result)
         }
         else if(opr == '*'){
@@ -29,4 +29,3 @@ if(isNaN(num1) || isNaN(num2)){
         alert('Invalid operator')
     }
 }
-
